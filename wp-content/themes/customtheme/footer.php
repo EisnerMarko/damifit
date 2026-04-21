81ea5a4d50a2bec22f39e250f92173510ace9901
EisnerMarko <mareis01@easv365.dk>
2026-04-21 16:05:33 +0200

    test update

--- wp-content/themes/customtheme/footer.php
+++ wp-content/themes/customtheme/footer.php
@@ -1,7 +1,7 @@
 <footer class="w-full bg-[#212529] text-white py-10 px-8 mt-10">
 
   <div class="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-8">
-  <h3>Mrdka</h3>
+  <h3>Finally works hopefully</h3>
   </div>
 </footer>
 
